30e1949f50d8e9b5bd51f219ef1633d15d759345
sagasagark1@gmail.com <sagarsagark1@gmail.com>
2017-07-03 22:43:36 +0530

    Initial code push

---
+++ AngularWithMVC/Scripts/Controller/angular-route.js
@@ -0,0 +1,49 @@
+﻿/// <reference path="../_references.js" />
+app.config(function ($stateProvider,$urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('home');
+    var about = {
+        name: 'about',
+        templateUrl: '/Home/About',
+        url: '/about'
+    };
+
+    var home = {
+        name: 'home',
+        url: '/home',
+        templateUrl: '/Home/Index'
+    };
+      
+    var contact = {
+        url: '/contact',
+        templateUrl: '/Home/Contact',
+        name : 'contact'
+    };
+
+    var register = {
+        name: 'register',
+        templateUrl: '/Account/Register',
+        url: '/register'
+    };
+
+    var login = {
+        name: 'login',
+        templateUrl: '/Account/Login',
+        url: '/login'
+    };
+
+    //Customer Registration
+    var custReg = {
+        name: 'customerRegistration',
+        templateUrl: '/Customer/Index',
+        url: '/customerRegistration'
+    };
+    $stateProvider.state(custReg);
+
+    $stateProvider.state(about);
+    $stateProvider.state(register);
+    $stateProvider.state(home);
+    $stateProvider.state(contact);
+    $stateProvider.state(login);
+
+});
---
+++ AngularWithMVC/Scripts/Controller/mainCntl.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="../_references.js" />
+app.controller('AngularController', function ($scope) {
+    debugger;
+    $scope.message = 'hello from angularjs';
+});
